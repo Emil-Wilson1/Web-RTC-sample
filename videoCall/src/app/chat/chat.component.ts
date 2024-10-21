a36dd94bb2cc192fbb47256c315a791a3bda821f
Emil Wilson <emilu551e@gmail.com>
2024-10-21 15:08:38 +0530

    local video setup and websocket server setup is completed

--- videoCall/src/app/chat/chat.component.ts
+++ videoCall/src/app/chat/chat.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
-import {MatGridListModule, MatGridTile} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { MatGridListModule, MatGridTile } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
-const mediaConstraints={
-  audio:true,
-  video:{width:720,height:540}
-}
+const mediaConstraints = {
+  audio: true,
+  video: { width: 720, height: 540 },
+};
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [MatGridListModule,MatGridTile,MatButtonModule],
+  imports: [MatGridListModule, MatGridTile, MatButtonModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent {
-   private localStream!:MediaStream
+  private localStream!: MediaStream;
+  @ViewChild('local_video') localVideo!:ElementRef
 
-   constructor(){}
+  constructor() {}
 
-   ngAfterViewInit():void{
-    
-   }
+  ngAfterViewInit(): void {
+    this.requestMediaDevices();
+  }
+
+  private async requestMediaDevices(): Promise<void> {
+    this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+    this.pauseLocalVideo()
+  }
+
+  pauseLocalVideo():void{
+    this.localStream.getTracks().forEach((track)=>{
+      track.enabled=false;
+    })
+    this.localVideo.nativeElement.srcObject=null
+  }
+
+  startLocalVideo():void{
+    this.localStream.getTracks().forEach((track)=>{
+      track.enabled=true;
+    })
+    this.localVideo.nativeElement.srcObject=this.localStream
+  }
 }
